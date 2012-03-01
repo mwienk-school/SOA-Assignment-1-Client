b2cea1942577eab9aa6d52969d2633b33a69c114
Mark Wienk <markwienk@gmail.com>
2012-03-01 16:08:08 +0100

    Updated

--- src/org/example/www/airline/AirlineClient.java
+++ src/org/example/www/airline/AirlineClient.java
@@ -32,7 +32,7 @@ public class AirlineClient extends JFrame {
 	// URL
 	private JLabel urlLabel = null;
 	private JTextField urlField = null;
-	//private final String defaultUrl = "http://localhost:8085/SOA_-_Assignment_1/services/airline"; //Monitor (enable in eclipse)
+	//private final String defaultUrl = "http://localhost:8085/SOA_-_Assignment_1/services/airline"; //Monitor
 	private final String defaultUrl = "http://localhost:8080/SOA_-_Assignment_1/services/airline";
 	private String url;
 	// Buttons
